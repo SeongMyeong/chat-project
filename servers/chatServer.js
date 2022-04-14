512c284cbbae3dfef3f3608de59930c69942e747
MasonMS <brightcastle91@gmail.com>
2022-04-14 11:23:54 +0900

    chat be init

---
+++ servers/chatServer.js
@@ -0,0 +1,14 @@
+// const daemon = require("./daemon");
+
+// const chatConfig = {
+//   port: 4001,
+//   namespace: "chat-space",
+// };
+
+// const nsp = daemon.io.of(chatConfig);
+
+// nsp.on("connection", (socket) => {});
+
+// daemon.server.listen(chatConfig.port, () => {
+//   console.log(`[masonms] chat server listen port ${chatConfig.port}`);
+// });
