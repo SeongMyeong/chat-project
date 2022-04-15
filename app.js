2872fe9808983387c451d42779def5f236f8d2a4
westkite1201 <westkite1201@gmail.com>
2022-04-15 18:09:45 +0900

    🔥 [FEAT] : 룸 테스트 및 유틸 추가

--- app.js
+++ app.js
@@ -1,40 +1,62 @@
-var app = require("express")();
-var server = require("http").createServer(app);
+var app = require('express')();
+var server = require('http').createServer(app);
+let { SOCKET_EVENT } = require('./util/constants');
 
-const cors = require("cors");
+const cors = require('cors');
 app.use(cors());
 
-var io = require("socket.io")(server, {
+var io = require('socket.io')(server, {
   cors: {
-    origin: "*",
+    origin: '*',
   },
 });
 
-app.set("view engine", "ejs"); // 렌더링 엔진 모드를 ejs로 설정
-app.set("views", __dirname + "/views"); // ejs이 있는 폴더를 지정
+app.set('view engine', 'ejs'); // 렌더링 엔진 모드를 ejs로 설정
+app.set('views', __dirname + '/views'); // ejs이 있는 폴더를 지정
 
-app.get("/", (req, res) => {
-  res.render("index"); // index.ejs을 사용자에게 전달
+app.get('/', (req, res) => {
+  res.render('index'); // index.ejs을 사용자에게 전달
 });
 
-io.on("connection", (socket) => {
-  console.log("[masonms] io connection");
+const namespace = io.of('room');
+namespace.use((socket, next) => {
+  // TODO jwt 검증 로직 필요
+  next();
+});
+
+namespace.on('connection', (socket) => {
+  const { room_id } = socket.handshake.query;
+  console.log('io connection ', room_id);
+  console.log(socket.rooms); // Set { <socket.id> }
+
+  socket.join(room_id);
+  console.log(socket.rooms); // Set { <socket.id>, "room1" }
+
+  console.log('[masonms] io connection');
   //연결이 들어오면 실행되는 이벤트
   // socket 변수에는 실행 시점에 연결한 상대와 연결된 소켓의 객체가 들어있다.
 
   //socket.emit으로 현재 연결한 상대에게 신호를 보낼 수 있다.
-  socket.emit("usercount", io.engine.clientsCount);
-
+  socket.emit('usercount', io.engine.clientsCount);
   // on 함수로 이벤트를 정의해 신호를 수신할 수 있다.
-  socket.on("message", (msg) => {
-    //msg에는 클라이언트에서 전송한 매개변수가 들어온다. 이러한 매개변수의 수에는 제한이 없다.
-    console.log("Message received: " + msg);
+  // socket.on('message', (msg) => {
+  //   //msg에는 클라이언트에서 전송한 매개변수가 들어온다. 이러한 매개변수의 수에는 제한이 없다.
+  //   console.log('Message received: ', msg);
 
-    // io.emit으로 연결된 모든 소켓들에 신호를 보낼 수 있다.
-    io.emit("message", msg);
+  //   // io.emit으로 연결된 모든 소켓들에 신호를 보낼 수 있다.
+  //   io.emit('message', msg);
+  // });
+  socket.on(SOCKET_EVENT.MESSAGE, (data) => {
+    //console.log('data =', data, socket);
+    console.log('소켓 메세지 on', socket.rooms); // Set { <socket.id>, "room1" }
+    const { id, message, user_name, room_id } = data;
+    namespace.in(room_id).emit(SOCKET_EVENT.MESSAGE, {
+      ...data,
+      time: new Date().getTime(),
+    });
   });
 });
 
 server.listen(3000, function () {
-  console.log("Listening on http://localhost:3000/");
+  console.log('Listening on http://localhost:3000/');
 });
---
+++ util/constants.js
@@ -0,0 +1,7 @@
+const SOCKET_EVENT = {
+  MESSAGE: 'message',
+  JOIN_ROOM: 'join room',
+  LEAVE_ROOM: 'leave room',
+  USER_COUNT: 'usercount',
+};
+module.exports = { SOCKET_EVENT };
